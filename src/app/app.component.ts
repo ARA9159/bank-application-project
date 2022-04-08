c829ffb0125b85b566491f906472c1a1bd94e52b
ARAVIND YELUBANDI <yaravind2106@gmail.com>
2022-04-08 14:09:17 +0530

    change-1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,14 +10,42 @@ export class AppComponent {
   title = 'CREATE ACCOUNT';
   aform:FormGroup;
   constructor(){
-    this.aform=new FormGroup({fname:new FormControl("ENTER FIRST NAME",[Validators.required]),
-  lname:new FormControl("ENTER LAST NAME",[Validators.required]),
-  age:new FormControl("ENTER AGE",[Validators.required,Validators.pattern('[0-9]+')]),
-  cnum:new FormControl("ENTER THE MOBILE NUMBERE",[Validators.required,Validators.pattern('[0-9]{10}')]),
-  anum:new FormControl("ENTER THE ADHAR NUMBER",[Validators.required,Validators.pattern('[0-9]+')]),
-  adres:new FormControl("ENTER THE DOOR NUMBER",[Validators.required]),
-  city:new FormControl("ENTER THE CITY NAME",[Validators.required]),
-  code:new FormControl("ENTER THE POSTAL CODE",[Validators.required,Validators.pattern('[0-9]{6}')])
+    this.aform=new FormGroup({fname:new FormControl(null,[Validators.required]),
+  lname:new FormControl(null,[Validators.required]),
+  age:new FormControl(null,[Validators.required,Validators.pattern('[0-9]{2}')]),
+  dob:new FormControl(null,[Validators.required]),
+  cnum:new FormControl(null,[Validators.required,Validators.pattern('[0-9]{10}')]),
+  anum:new FormControl(null,[Validators.required,Validators.pattern('[0-9]+')]),
+  adres:new FormControl(null,[Validators.required]),
+  cty:new FormControl(null,[Validators.required]),
+  code:new FormControl(null,[Validators.required,Validators.pattern('[0-9]{6}')])
   });
+  }
+  get fname(){
+    return this.aform.get('fname');
+  }
+  get lname(){
+    return this.aform.get('lname');
+  }
+  get age(){
+    return this.aform.get('age');
+  }
+  get dob(){
+    return this.aform.get('dob');
+  }
+  get cnum(){
+    return this.aform.get('cnum');
+  }
+  get anum(){
+    return this.aform.get('anum');
+  }
+  get adres(){
+    return this.aform.get('adres');
+  }
+  get cty(){
+    return this.aform.get('cty');
+  }
+  get code(){
+    return this.aform.get('code');
   }
 }
